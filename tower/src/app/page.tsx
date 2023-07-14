c3fd7660e332e1be5b466942ff599d3596b2facb
Aleksey Filatev <afilatev@plesk.com>
2023-07-14 03:49:10 +0300

    Add missed TypeScript declrataion to fix ability to build application statically. .

--- tower/src/app/page.tsx
+++ tower/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     const { data } = useSWR('http://localhost:8080/api/v1/hello', (api: string) => fetch(api).then(res => res.json()));
     return <div className={"text-sm font-mono box-content p-4"}>
         {data ? (
-            data.map(title => <Card title={title}></Card>)
+            data.map((title: string) => <Card key={title} title={title}></Card>)
         ) : "Loading"}
     </div>
 }
