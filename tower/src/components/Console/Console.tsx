0767bdc22313fc0acf028e97bcceda9db6e94428
agxmeister <agxmeister@gmail.com>
2023-09-07 01:21:25 +0300

    Add the ability to edit the current console command in the middle.

--- tower/src/components/Console/Console.tsx
+++ tower/src/components/Console/Console.tsx
@@ -11,22 +11,26 @@ export default function Console({context, setters}: ConsoleProps)
 {
     const [lines, setLines] = useState<string[]>(['> ']);
     const [index, setIndex] = useState<number>(0);
+    const [position, setPosition] = useState<number>(0);
     const inputLines = lines
         .filter((value: string, index: number) => index > 0 && value.startsWith('> '))
         .filter((value: string, index: number, array: string[]) => array.indexOf(value) === index);
     inputLines.unshift('> ');
     let inputIndex = index;
+    let inputPosition = position;
     const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         handleInput(event.key).then(() => {
             setLines([...lines]);
             setIndex(inputIndex);
+            setPosition(inputPosition);
         })
         event.preventDefault();
     }
 
     const handleInput = async (key: string) => {
         if (key.length === 1) {
-            lines[0] = lines[0] + key;
+            lines[0] = lines[0].slice(0, inputPosition + 2) + key + lines[0].slice(inputPosition + 2);
+            inputPosition++;
         } else {
             switch (key) {
                 case 'Enter':
@@ -36,10 +40,12 @@ export default function Console({context, setters}: ConsoleProps)
                     lines.unshift(...await run(lines[0].slice(2), context, setters));
                     lines.unshift('> ');
                     inputIndex = 0;
+                    inputPosition = 0;
                     break;
                 case 'Backspace':
                     if (lines[0].length > 2) {
-                        lines[0] = lines[0].slice(0, -1);
+                        lines[0] = lines[0].slice(0, inputPosition + 1) + lines[0].slice(inputPosition + 2);
+                        inputPosition--;
                     }
                     break;
                 case 'ArrowUp':
@@ -53,6 +59,16 @@ export default function Console({context, setters}: ConsoleProps)
                         inputIndex--;
                     }
                     lines[0] = inputLines[inputIndex];
+                    break;
+                case 'ArrowLeft':
+                    if (inputPosition > 0) {
+                        inputPosition--;
+                    }
+                    break;
+                case 'ArrowRight':
+                    if (inputPosition < lines[0].length - 2) {
+                        inputPosition++;
+                    }
                     break;
                 default:
                     break;
@@ -73,10 +89,13 @@ export default function Console({context, setters}: ConsoleProps)
                         null
                     }
                     {index === 0 ?
-                        <span className={styles.command}>{line}</span> :
+                        <>
+                            <span className={styles.command}>{line.slice(0, position + 2)}</span>
+                            <span className={styles.cursor}>{position + 2 < line.length ? line.slice(position + 2, position + 3) : <>&nbsp;</>}</span>
+                            <span className={styles.command}>{line.slice(position + 3)}</span>
+                        </> :
                         null
                     }
-                    {index === 0 ? <span className={styles.cursor}>&nbsp;</span> : null}
                 </div>
             )}
         </div>
