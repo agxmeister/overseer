3b00c04d9425a03eb9b54ce1c4f4e85049f3607f
agxmeister <agxmeister@gmail.com>
2023-09-10 02:31:13 +0300

    Cover by tests the schedule builder when it runs with the work limiting strategy.

--- watch/tests/Unit/BuilderTest.php
+++ watch/tests/Unit/BuilderTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 use Exception;
 use Codeception\Test\Unit;
 use Watch\Schedule\Builder;
+use Watch\Schedule\Link;
+use Watch\Schedule\Node;
 use Watch\Schedule\Strategy\Strategy;
 
 class BuilderTest extends Unit
@@ -11,7 +13,7 @@ class BuilderTest extends Unit
     /**
      * @throws Exception
      */
-    public function testGetSchedule()
+    public function testGetScheduleUnlimited()
     {
         $builder = new Builder();
         $schedule = $builder->getSchedule(
@@ -62,6 +64,61 @@ class BuilderTest extends Unit
                     'estimatedBeginDate' => '2023-09-01',
                     'estimatedEndDate' => '2023-09-08',
                 ]
+            ],
+            $schedule,
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetScheduleLimited()
+    {
+        $builder = new Builder();
+        $schedule = $builder->getSchedule(
+            [
+                [
+                    'key' => 'K-01',
+                    'estimatedDuration' => '4',
+                    'estimatedBeginDate' => null,
+                    'estimatedEndDate' => null,
+                    'links' => [
+                        'inward' => [],
+                    ],
+                ], [
+                    'key' => 'K-02',
+                    'estimatedDuration' => '4',
+                    'estimatedBeginDate' => null,
+                    'estimatedEndDate' => null,
+                    'links' => [
+                        'inward' => [],
+                    ],
+                ],
+            ],
+            '2023-09-09',
+            $this->makeEmpty(Strategy::class, ['schedule' => function (Node $node) {
+                $preceders = $node->getPreceders();
+                $preceders[1]->unprecede($node);
+                $preceders[1]->precede($preceders[0], Link::TYPE_SCHEDULE);
+            }]),
+        );
+        $this->assertEquals(
+            [
+                [
+                    'key' => 'K-01',
+                    'estimatedBeginDate' => '2023-09-05',
+                    'estimatedEndDate' => '2023-09-08',
+                    'links' => [
+                        'inward' => ['K-02'],
+                    ],
+                ], [
+                    'key' => 'K-02',
+                    'estimatedBeginDate' => '2023-09-01',
+                    'estimatedEndDate' => '2023-09-04',
+                    'links' => [
+                        'outward' => ['K-01'],
+                    ],
+                ],
             ],
             $schedule,
         );
