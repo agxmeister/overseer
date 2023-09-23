4bd61ab15d19bc7124807f1c3657afc9fcf7c121
agxmeister <agxmeister@gmail.com>
2023-09-24 02:46:21 +0300

    Move the formatting of the schedule to a separate method.

--- watch/src/Schedule/Builder.php
+++ watch/src/Schedule/Builder.php
@@ -6,63 +6,14 @@ use Watch\Schedule\Strategy\Strategy;
 
 class Builder
 {
+    public function __construct(private Formatter $formatter)
+    {
+    }
+
     public function getSchedule(array $issues, string $date, Strategy $strategy): array
     {
         $milestone = Utils::getMilestone($issues);
         $strategy->schedule($milestone);
-        $nodes = $milestone->getPreceders(true);
-
-        return array_map(function (array $issue) use ($nodes, $date) {
-            $result = [
-                'key' => $issue['key'],
-                'estimatedBeginDate' => $issue['estimatedBeginDate'],
-                'estimatedEndDate' => $issue['estimatedEndDate'],
-            ];
-
-            /** @var Node $node */
-            $node = array_reduce($nodes, fn($acc, Node $node) => $node->getName() === $issue['key'] ? $node : $acc);
-            if (is_null($node)) {
-                return $result;
-            }
-
-            $distance = $node->getDistance();
-            $completion = $node->getCompletion();
-            $startDate = (new \DateTime($date))->modify("-{$distance} day");
-            $finishDate = (new \DateTime($date))->modify("-{$completion} day");
-            $result['estimatedBeginDate'] = $startDate->format("Y-m-d");
-            $result['estimatedEndDate'] = $finishDate->format("Y-m-d");
-
-            $inwardLinks = array_values(array_map(
-                fn(Node $follower) => [
-                    'key' => $follower->getName(),
-                    'type' => 'Follows',
-                ],
-                array_filter(
-                    $node->getFollowers([Link::TYPE_SCHEDULE]),
-                    fn(Node $node) => !is_a($node, Milestone::class)
-                )
-            ));
-            $outwardLinks = array_values(array_map(
-                fn(Node $preceder) => [
-                    'key' => $preceder->getName(),
-                    'type' => 'Follows',
-                ],
-                array_filter(
-                    $node->getPreceders(false, [Link::TYPE_SCHEDULE]),
-                    fn(Node $node) => !is_a($node, Milestone::class)
-                )
-            ));
-            if ($inwardLinks || $outwardLinks) {
-                $result['links'] = [];
-                if ($inwardLinks) {
-                    $result['links']['inward'] = $inwardLinks;
-                }
-                if ($outwardLinks) {
-                    $result['links']['outward'] = $outwardLinks;
-                }
-            }
-
-            return $result;
-        }, $issues);
+        return $this->formatter->getSchedule($issues, $milestone, $date);
     }
 }
