5f076991bd4036b92c0e24bcb78741e1e3f2008d
agxmeister <agxmeister@gmail.com>
2023-10-18 00:05:02 +0300

    Generate the test issues data from the description.

--- watch/tests/Unit/Schedule/DirectorTest.php
+++ watch/tests/Unit/Schedule/DirectorTest.php
@@ -18,46 +18,11 @@ class DirectorTest extends Unit
     {
         $director = new Director(new Builder());
         $schedule = $director->create(
-            [
-                [
-                    'key' => 'K-01',
-                    'duration' => '4',
-                    'begin' => null,
-                    'end' => null,
-                    'links' => [
-                        'inward' => [],
-                        'outward' => [
-                            [
-                                'key' => 'K-02',
-                                'type' => 'sequence',
-                            ],
-                        ],
-                    ],
-                ], [
-                    'key' => 'K-02',
-                    'duration' => '4',
-                    'begin' => null,
-                    'end' => null,
-                    'links' => [
-                        'inward' => [
-                            [
-                                'key' => 'K-01',
-                                'type' => 'sequence',
-                            ],
-                        ],
-                        'outward' => [],
-                    ],
-                ], [
-                    'key' => 'K-03',
-                    'duration' => '7',
-                    'begin' => null,
-                    'end' => null,
-                    'links' => [
-                        'inward' => [],
-                        'outward' => [],
-                    ],
-                ],
-            ],
+            $this->getIssues('
+                K-01     xxxx
+                K-02 xxxx     K-01
+                K-03 xxxxxxx
+            '),
             new DateTime('2023-09-21'),
             $this->makeEmpty(Strategy::class),
         );
@@ -130,27 +95,10 @@ class DirectorTest extends Unit
     {
         $builder = new Director(new Builder());
         $schedule = $builder->create(
-            [
-                [
-                    'key' => 'K-01',
-                    'duration' => '4',
-                    'begin' => null,
-                    'end' => null,
-                    'links' => [
-                        'inward' => [],
-                        'outward' => [],
-                    ],
-                ], [
-                    'key' => 'K-02',
-                    'duration' => '4',
-                    'begin' => null,
-                    'end' => null,
-                    'links' => [
-                        'inward' => [],
-                        'outward' => [],
-                    ],
-                ],
-            ],
+            $this->getIssues('
+                K-01 xxxx
+                K-02 xxxx
+            '),
             new DateTime('2023-09-21'),
             new Test(),
         );
@@ -195,5 +143,44 @@ class DirectorTest extends Unit
             ],
             $schedule,
         );
+    }
+
+    private function getIssues($description): array
+    {
+        $lines = [...array_filter(array_map(fn($line) => trim($line), explode("\n", $description)), fn($line) => strlen($line) > 0)];
+
+        $issues = [];
+        $links = [];
+        foreach ($lines as $line) {
+            $issueData = preg_split("/\s+/", $line);
+            $key = $issueData[0];
+            $duration = strlen($issueData[1]);
+            $linkName = $issueData[2] ?? null;
+            $issues[$key] = [
+                'key' => $key,
+                'duration' => $duration,
+                'begin' => null,
+                'end' => null,
+                'links' => [
+                    'inward' => [],
+                    'outward' => [],
+                ]
+            ];
+            if (!is_null($linkName)) {
+                $links[] = ['from' => $key, 'to' => $linkName];
+            }
+        }
+        foreach($links as $link) {
+            $issues[$link['from']]['links']['inward'][] = [
+                'key' => $link['to'],
+                'type' => 'sequence',
+            ];
+            $issues[$link['to']]['links']['outward'][] = [
+                'key' => $link['from'],
+                'type' => 'sequence',
+            ];
+        }
+
+        return array_values($issues);
     }
 }
