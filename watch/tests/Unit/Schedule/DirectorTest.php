d9cc2813742e61404cf3f7f87b4dc5c63106c269
agxmeister <agxmeister@gmail.com>
2023-10-22 00:06:36 +0300

    Move the schedule director test data to the data provider.

--- watch/tests/Unit/Schedule/DirectorTest.php
+++ watch/tests/Unit/Schedule/DirectorTest.php
@@ -3,7 +3,6 @@ namespace Tests\Unit\Schedule;
 
 use Codeception\Test\Unit;
 use DateTime;
-use Exception;
 use Tests\Support\Utils;
 use Watch\Schedule\Builder;
 use Watch\Schedule\Director;
@@ -13,56 +12,77 @@ use Watch\Schedule\Strategy\Test;
 class DirectorTest extends Unit
 {
     /**
-     * @throws Exception
+     * @dataProvider dataGetScheduleUnlimited
      */
-    public function testGetScheduleUnlimited()
+    public function testGetScheduleUnlimited($issues, $schedule)
     {
         $director = new Director(new Builder());
-        $schedule = $director->create(
-            Utils::getIssues('
-                K-01          |    ....|
-                K-02          |....    |          & K-01
-                K-03          |....... |
-            '),
-            new DateTime('2023-09-21'),
-            $this->makeEmpty(Strategy::class),
-        );
-
-        $this->assertEquals(
-            Utils::getSchedule('
-                finish        |                !| # 2023-09-21
-                finish-buffer |            ____ | @ finish
-                K-01          |        xxxx     | @ finish-buffer
-                K-03-buffer   |        ____     | @ finish-buffer
-                K-02          |    xxxx         | & K-01
-                K-03          | *******         | @ K-03-buffer
-            '),
-            $schedule,
-        );
+        $date = new DateTime('2023-09-21');
+        $strategy = $this->makeEmpty(Strategy::class);
+        $this->assertEquals($schedule, $director->create($issues, $date, $strategy));
     }
 
     /**
-     * @throws Exception
+     * @dataProvider dataGetScheduleLimited
      */
-    public function testGetScheduleLimited()
+    public function testGetScheduleLimited($issues, $schedule)
     {
-        $builder = new Director(new Builder());
-        $schedule = $builder->create(
-            Utils::getIssues('
-                K-01          |....|
-                K-02          |....|
-            '),
-            new DateTime('2023-09-21'),
-            new Test(),
-        );
-        $this->assertEquals(
-            Utils::getSchedule('
-                finish        |                !| # 2023-09-21
-                finish-buffer |            ____ | @ finish
-                K-01          |        xxxx     | @ finish-buffer
-                K-02          |    xxxx         | @ K-01
-            '),
-            $schedule,
-        );
+        $director = new Director(new Builder());
+        $date = new DateTime('2023-09-21');
+        $strategy = new Test();
+        $this->assertEquals($schedule, $director->create($issues, $date, $strategy));
+    }
+
+    protected function dataGetScheduleUnlimited(): array
+    {
+        return [
+            /*[
+                Utils::getIssues('
+                    K-01          |    ....        |
+                    K-02          |....            |          & K-01
+                    K-03          |.......         |
+                '),
+                Utils::getSchedule('
+                    finish        |               !| # 2023-09-21
+                    finish-buffer |           ____ | @ finish
+                    K-01          |       xxxx     | @ finish-buffer
+                    K-03-buffer   |       ____     | @ finish-buffer
+                    K-02          |   xxxx         | & K-01
+                    K-03          |*******         | @ K-03-buffer
+                '),
+            ],*/ [
+                Utils::getIssues('
+                    K-01          |       ....       |
+                    K-03          |.......           | & K-01
+                    K-02          |....              | & K-01
+                '),
+                Utils::getSchedule('
+                    finish        |                 !| # 2023-09-21
+                    finish-buffer |           ______ | @ finish
+                    K-01          |       xxxx       | @ finish-buffer
+                    K-03          |xxxxxxx           | & K-01
+                    K-02-buffer   |     __           | @ K-01
+                    K-02          | ****             | & K-01, @ K-02-buffer
+                '),
+            ],
+        ];
+    }
+
+    protected function dataGetScheduleLimited(): array
+    {
+        return [
+            [
+                Utils::getIssues('
+                    K-01          |....         |
+                    K-02          |....         |
+                '),
+                Utils::getSchedule('
+                    finish        |            !| # 2023-09-21
+                    finish-buffer |        ____ | @ finish
+                    K-01          |    xxxx     | @ finish-buffer
+                    K-02          |xxxx         | @ K-01
+                '),
+            ],
+        ];
     }
 }
