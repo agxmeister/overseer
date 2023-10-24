d287521d4116228244eae6aed2e3a700ecb557c8
agxmeister <agxmeister@gmail.com>
2023-10-25 01:02:08 +0300

    Make assertions in the schedule tests more precise.

--- watch/tests/Unit/Schedule/DirectorTest.php
+++ watch/tests/Unit/Schedule/DirectorTest.php
@@ -20,9 +20,7 @@ class DirectorTest extends Unit
         $director = new Director(new Builder());
         $date = new DateTime('2023-09-21');
         $strategy = $this->makeEmpty(Strategy::class);
-        $actualSchedule = $director->create($issues, $date, $strategy);
-        $this->assertEqualsCanonicalizing($schedule, $actualSchedule);
-        $this->assertEquals($schedule['criticalChain'], $actualSchedule['criticalChain']);
+        $this->assertSchedule($schedule, $director->create($issues, $date, $strategy));
     }
 
     /**
@@ -33,9 +31,7 @@ class DirectorTest extends Unit
         $director = new Director(new Builder());
         $date = new DateTime('2023-09-21');
         $strategy = new Test();
-        $actualSchedule = $director->create($issues, $date, $strategy);
-        $this->assertEqualsCanonicalizing($schedule, $actualSchedule);
-        $this->assertEquals($schedule['criticalChain'], $actualSchedule['criticalChain']);
+        $this->assertSchedule($schedule, $director->create($issues, $date, $strategy));
     }
 
     /**
@@ -46,9 +42,26 @@ class DirectorTest extends Unit
         $director = new Director(new Builder());
         $date = new DateTime('2023-09-21');
         $strategy = new Basic();
-        $actualSchedule = $director->create($issues, $date, $strategy);
-        $this->assertEqualsCanonicalizing($schedule, $actualSchedule);
-        $this->assertEquals($schedule['criticalChain'], $actualSchedule['criticalChain']);
+        $this->assertSchedule($schedule, $director->create($issues, $date, $strategy));
+    }
+
+    protected function assertSchedule($expected, $actual)
+    {
+        $this->assertSameSize($expected, $actual, 'Number of volumes in the schedule is differ from expected.');
+        foreach (['issues', 'buffers', 'links'] as $volume) {
+            $this->assertScheduleVolume($expected[$volume], $actual[$volume], $volume);
+        }
+        $this->assertEquals($expected['criticalChain'], $actual['criticalChain'], 'Critical chain of the schedule is differ from expected.');
+    }
+
+    protected function assertScheduleVolume($expected, $actual, $volume)
+    {
+        $this->assertSameSize($expected, $actual, "Number of items in volume '{$volume}' is differ from expected.");
+        usort($expected, fn($a, $b) => $a < $b ? -1 : ($a > $b ? 1 : 0));
+        usort($actual, fn($a, $b) => $a < $b ? -1 : ($a > $b ? 1 : 0));
+        for ($i = 0; $i < sizeof($actual); $i++) {
+            $this->assertEquals($expected[$i], $actual[$i], "Items in volume '{$volume}' are mismatched.");
+        }
     }
 
     protected function dataGetScheduleUnlimited(): array
@@ -134,9 +147,9 @@ class DirectorTest extends Unit
                 '),
             ], [
                 Utils::getIssues('
-                    K-01          |....              |
-                    K-02          |....              |
-                    K-03          |....              |
+                    K-01          |....        |
+                    K-02          |....        |
+                    K-03          |....        |
                 '),
                 Utils::getSchedule('
                     finish-buffer |        ____| @ finish
@@ -145,6 +158,31 @@ class DirectorTest extends Unit
                     K-03-buffer   |      __    | @ finish-buffer
                     K-03          |  ****      | @ K-03-buffer
                     finish                     ^ # 2023-09-21
+                '),
+            ], [
+                Utils::getIssues('
+                    K-01          |....        |
+                    K-02          |....        | & K-01
+                '),
+                Utils::getSchedule('
+                    finish-buffer |        ____| @ finish
+                    K-01          |    xxxx    | @ finish-buffer
+                    K-02          |xxxx        | & K-01
+                    finish                     ^ # 2023-09-21
+                '),
+            ], [
+                Utils::getIssues('
+                    K-01          |....          |
+                    K-02          |....          | & K-01
+                    K-03          |....          | & K-01
+                '),
+                Utils::getSchedule('
+                    finish-buffer |          ____| @ finish
+                    K-01          |      xxxx    | @ finish-buffer
+                    K-02          |  xxxx        | & K-01
+                    K-03-buffer   |    __        | @ K-01
+                    K-03          |****          | & K-01, @ K-03-buffer
+                    finish                       ^ # 2023-09-21
                 '),
             ],
         ];
