146e26d4d1ce1d816dbe365980734c72454e1205
agxmeister <agxmeister@gmail.com>
2023-11-11 14:36:02 +0200

    Adapt the left-to-right scheduling strategy to take into account the sequence of issues.

--- watch/src/Schedule/Builder/Strategy/Schedule/LeftToRight.php
+++ watch/src/Schedule/Builder/Strategy/Schedule/LeftToRight.php
@@ -4,12 +4,31 @@ namespace Watch\Schedule\Builder\Strategy\Schedule;
 
 use Watch\Schedule\Builder\ScheduleStrategy;
 use Watch\Schedule\Model\Buffer;
+use Watch\Schedule\Model\Issue;
 use Watch\Schedule\Model\Node;
 
 readonly class LeftToRight implements ScheduleStrategy
 {
     public function apply(Node $milestone): void
     {
+        $issues = array_filter($milestone->getPreceders(true), fn(Node $node) => $node instanceof Issue);
+        usort(
+            $issues,
+            fn(Node $a, Node $b) =>
+                $a->getDistance() < $b->getDistance() ? 1 : ($a->getDistance() > $b->getDistance() ? -1 : 0)
+        );
+        foreach ($issues as $issue) {
+            $maxPrecedersEndDate = array_reduce(
+                $issue->getPreceders(),
+                fn($acc, Node $node) => max($acc, $node->getAttribute('end')),
+            );
+            if (is_null($maxPrecedersEndDate)) {
+                continue;
+            }
+            $date = (new \DateTimeImmutable($maxPrecedersEndDate));
+            $issue->setAttribute('begin', $date->modify("1 day")->format("Y-m-d"));
+            $issue->setAttribute('end', $date->modify("{$issue->getLength()} day")->format("Y-m-d"));
+        }
         foreach (array_filter($milestone->getPreceders(true), fn(Node $node) => $node instanceof Buffer) as $buffer) {
             $date = new \DateTimeImmutable(max(array_map(
                 fn(Node $node) => $node->getAttribute('end') ?? null,
--- watch/tests/Unit/Schedule/Director/CorrectiveFromScratchDirectorTest.php
+++ watch/tests/Unit/Schedule/Director/CorrectiveFromScratchDirectorTest.php
@@ -5,15 +5,32 @@ use Tests\Support\Utils;
 use Watch\Schedule\Builder\Context;
 use Watch\Schedule\Builder\FromScratch as FromScratchBuilder;
 use Watch\Schedule\Builder\Strategy\Limit\Corrective as CorrectiveLimitStrategy;
+use Watch\Schedule\Builder\Strategy\Schedule\LeftToRight as LeftToRightScheduleStrategy;
 use Watch\Schedule\Builder\Strategy\Schedule\RightToLeft as RightToLeftScheduleStrategy;
 use Watch\Schedule\Director;
 
 class CorrectiveFromScratchDirectorTest extends AbstractDirectorTest
 {
     /**
-     * @dataProvider dataBuild
+     * @dataProvider dataBuildLeftToRight
      */
-    public function testBuild($issuesDescription, $scheduleDescription)
+    public function testBuildLeftToRight($issuesDescription, $scheduleDescription)
+    {
+        $director = new Director(
+            new FromScratchBuilder(
+                new Context(Utils::getNowDate($scheduleDescription)),
+                Utils::getIssues($issuesDescription),
+                new LeftToRightScheduleStrategy(),
+                new CorrectiveLimitStrategy(2),
+            )
+        );
+        $this->assertSchedule(Utils::getSchedule($scheduleDescription), $director->build()->release());
+    }
+
+    /**
+     * @dataProvider dataBuildRightToLeft
+     */
+    public function testBuildRightToLeft($issuesDescription, $scheduleDescription)
     {
         $director = new Director(
             new FromScratchBuilder(
@@ -26,13 +43,14 @@ class CorrectiveFromScratchDirectorTest extends AbstractDirectorTest
         $this->assertSchedule(Utils::getSchedule($scheduleDescription), $director->build()->release());
     }
 
-    protected function dataBuild(): array
+    protected function dataBuildLeftToRight(): array
     {
         return [
             ['
-                K-01          |......         |
-                K-02          |.....          |
-                K-03          |....           |
+                K-01          |   ******      |
+                K-02          |  *****        |
+                K-03          |****           |
+                                              ^ # 2023-09-21
             ', '
                 finish-buffer |          _____| @ finish
                 K-01          |    xxxxxx     | @ finish-buffer
@@ -40,11 +58,17 @@ class CorrectiveFromScratchDirectorTest extends AbstractDirectorTest
                 K-02          |  *****        | @ K-02-buffer
                 K-03          |xxxx           | @ K-01
                 finish        ^               ^ # 2023-09-21
-            '], ['
-                K-01          |   ******      |
-                K-02          |  *****        |
-                K-03          |****           |
-                                              ^ # 2023-09-21
+            '],
+        ];
+    }
+
+    protected function dataBuildRightToLeft(): array
+    {
+        return [
+            ['
+                K-01          |......         |
+                K-02          |.....          |
+                K-03          |....           |
             ', '
                 finish-buffer |          _____| @ finish
                 K-01          |    xxxxxx     | @ finish-buffer
