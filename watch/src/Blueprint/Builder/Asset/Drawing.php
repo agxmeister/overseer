bb00787d26a4b3da90929f2f6734e1dcc03ff1aa
agxmeister <agxmeister@gmail.com>
2024-07-17 20:07:25 +0300

    Add the ability to retrieve several strokes by a single pattern from a blueprint drawing at once.

--- watch/src/Blueprint/Builder/Asset/Drawing.php
+++ watch/src/Blueprint/Builder/Asset/Drawing.php
@@ -17,12 +17,25 @@ readonly class Drawing
         );
     }
 
-    public function getStroke(Parser $parser, string $attributesPartName): ?Stroke
+    public function getStroke(Parser $parser, string $attributesPartName, ...$defaults): ?Stroke
     {
         $match = current($parser->getMatches($this->strokes));
         if ($match === false) {
             return null;
         }
+        return $this->createStroke($match, $attributesPartName, ...$defaults);
+    }
+
+    public function getStrokes(Parser $parser, string $attributesPartName, ...$defaults): ?array
+    {
+        return array_map(
+            fn(array $match) => $this->createStroke($match, $attributesPartName, ...$defaults),
+            $parser->getMatches($this->strokes),
+        );
+    }
+
+    private function createStroke($match, $attributesPartName, ...$defaults): Stroke
+    {
         [$parts, $offsets] = $match;
         $attributes = $parts[$attributesPartName] ?? '';
         $filteredParts = array_filter(
@@ -30,6 +43,6 @@ readonly class Drawing
             fn(string $key) => $key !== $attributesPartName,
             ARRAY_FILTER_USE_KEY
         );
-        return new Stroke($filteredParts, $attributes, $offsets);
+        return new Stroke($filteredParts, $attributes, $offsets, ...$defaults);
     }
 }
