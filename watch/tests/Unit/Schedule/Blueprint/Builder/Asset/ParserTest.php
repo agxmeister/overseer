63d3551deb896b5b726683ef67f4d31053de645f
agxmeister <agxmeister@gmail.com>
2024-08-07 17:29:51 +0300

    Simplify the declaration of expected results in tests for the blueprint drawings parser.

--- watch/tests/Unit/Schedule/Blueprint/Builder/Asset/ParserTest.php
+++ watch/tests/Unit/Schedule/Blueprint/Builder/Asset/ParserTest.php
@@ -12,7 +12,7 @@ class ParserTest extends Unit
     public function testGetMatch($pattern, $line, $expected)
     {
         $parser = new Parser($pattern);
-        $this->assertEquals($expected, $parser->getMatch($line));
+        $this->assertEquals($this->getExpectedMatch($expected), $parser->getMatch($line));
     }
 
     static public function dataGetMatch(): array
@@ -22,57 +22,45 @@ class ParserTest extends Unit
                 '/\s*(((((?<project>[\w\-]+)(#(?<milestone>[\w\-]+))?)\/)?(?<type>[\w\-]+)\/)?(?<key>[\w\-]+))\s+(?<modifier>[~+\-])?(?<beginMarker>\|)(?<track>[x*.\s]*)(?<endMarker>\|)\s*(?<attributes>.*)/',
                 ' K-01 |xxx   | @ M-01 ',
                 [
-                    [
-                        'project' => null,
-                        'milestone' => null,
-                        'type' => null,
-                        'key' => 'K-01',
-                        'modifier' => null,
-                        'beginMarker' => '|',
-                        'track' => 'xxx   ',
-                        'endMarker' => '|',
-                        'attributes' => '@ M-01 ',
-                    ],
-                    [
-                        'project' => -1,
-                        'milestone' => -1,
-                        'type' => -1,
-                        'key' => 1,
-                        'modifier' => -1,
-                        'beginMarker' => 6,
-                        'track' => 7,
-                        'endMarker' => 13,
-                        'attributes' => 15,
-                    ],
+                    'project' => null,
+                    'milestone' => null,
+                    'type' => null,
+                    'key' => ['K-01', 1],
+                    'modifier' => null,
+                    'beginMarker' => ['|', 6],
+                    'track' => ['xxx   ', 7],
+                    'endMarker' => ['|', 13],
+                    'attributes' => ['@ M-01 ', 15],
                 ],
             ], [
                 '/\s*(((((?<project>[\w\-]+)(#(?<milestone>[\w\-]+))?)\/)?(?<type>[\w\-]+)\/)?(?<key>[\w\-]+))\s+(?<modifier>[~+\-])?(?<beginMarker>\|)(?<track>[x*.\s]*)(?<endMarker>\|)\s*(?<csv_attributes>.*)/',
                 ' K-01 |xxx   | @ M-01 ',
                 [
-                    [
-                        'project' => null,
-                        'milestone' => null,
-                        'type' => null,
-                        'key' => 'K-01',
-                        'modifier' => null,
-                        'beginMarker' => '|',
-                        'track' => 'xxx   ',
-                        'endMarker' => '|',
-                        'attributes' => ['@ M-01'],
-                    ],
-                    [
-                        'project' => -1,
-                        'milestone' => -1,
-                        'type' => -1,
-                        'key' => 1,
-                        'modifier' => -1,
-                        'beginMarker' => 6,
-                        'track' => 7,
-                        'endMarker' => 13,
-                        'attributes' => 15,
-                    ],
+                    'project' => null,
+                    'milestone' => null,
+                    'type' => null,
+                    'key' => ['K-01', 1],
+                    'modifier' => null,
+                    'beginMarker' => ['|', 6],
+                    'track' => ['xxx   ', 7],
+                    'endMarker' => ['|', 13],
+                    'attributes' => [['@ M-01'], 15],
                 ],
             ],
+        ];
+    }
+
+    private function getExpectedMatch($expected): array
+    {
+        return [
+            array_map(
+                fn($value) => $value[0] ?? null,
+                $expected,
+            ),
+            array_map(
+                fn($value) => $value[1] ?? -1,
+                $expected,
+            ),
         ];
     }
 }
